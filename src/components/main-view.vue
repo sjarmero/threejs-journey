e5772f49656b434bfaacaa1becb39fd2e26d43db
Sergio Juan Armero <sjarmero@gmail.com>
2026-03-08 10:06:05 +0100

    01-06 manual

--- src/components/main-view.vue
+++ src/components/main-view.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import * as THREE from 'three';
-import {onMounted, ref} from "vue";
+import {onMounted, ref} from 'vue';
+import {WebGLRenderer} from 'three';
 
 const width = 800;
 const height = 600;
@@ -12,22 +13,57 @@ const mesh = new THREE.Mesh(geometry, material);
 const scene = new THREE.Scene();
 scene.add(mesh);
 
-const camera = new THREE.PerspectiveCamera(75, width / height);
+const aspectRatio = width / height;
+const camera = new THREE.PerspectiveCamera(75, aspectRatio);
 camera.position.z = 3;
+camera.lookAt(mesh.position);
 scene.add(camera);
 
 const canvas = ref<HTMLCanvasElement | null>(null);
+let canvasOffset: {x: number, y:number};
+let renderer: WebGLRenderer;
 
 onMounted(()=> {
   if (canvas.value) {
-    const renderer = new THREE.WebGLRenderer({
+    canvasOffset = {
+      x: canvas.value.offsetLeft,
+      y: canvas.value.offsetTop
+    };
+
+    renderer = new THREE.WebGLRenderer({
       canvas: canvas.value,
-    })
+    });
 
     renderer.setSize(width, height);
     renderer.render(scene, camera);
+
+    tick();
+
+    canvas.value.addEventListener('mousemove', onMouseMove);
   }
 });
+
+const cursor = {x: 0, y: 0};
+const RANGE_OFFSET = -0.5;
+function onMouseMove(event: MouseEvent) {
+  cursor.x = ((event.clientX - canvasOffset.x) / width) + RANGE_OFFSET;
+  cursor.y = ((event.clientY - canvasOffset.y) / height) + RANGE_OFFSET;
+
+  console.log(cursor);
+}
+
+const clock = new THREE.Clock();
+const fullRev = 2 * Math.PI;
+function tick() {
+  // mesh.rotation.y = clock.getElapsedTime();
+  camera.position.x = Math.sin(cursor.x * fullRev) * 3;
+  camera.position.z = Math.cos(cursor.x * fullRev) * 3;
+  camera.position.y = cursor.y * 5;
+  camera.lookAt(mesh.position);
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(tick);
+}
 </script>
 <template>
   <div>
