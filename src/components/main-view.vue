6b603abf13368804bb061fa3d2803563630299a5
Sergio Juan Armero <sjarmero@gmail.com>
2026-03-14 16:53:08 +0100

    01-10

--- src/components/main-view.vue
+++ src/components/main-view.vue
@@ -18,7 +18,8 @@ const material = new THREE.MeshBasicMaterial({
   map: texture,
 });
 
-const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const mesh = new THREE.Mesh(geometry, material);
 
 const scene = new THREE.Scene();
 scene.add(mesh);
